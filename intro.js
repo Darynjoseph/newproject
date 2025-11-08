29b07516ce222e2d32f260722a2cc343b0a41657
Darynjoseph <162087710+Darynjoseph@users.noreply.github.com>
2025-11-08 11:10:17 +0300

    Add JavaScript code and project files

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Joseph Mutinda";
+const age = 20;
+const favouriteLanguage = "Java";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favourite programming language is ${favouriteLanguage}.`);
